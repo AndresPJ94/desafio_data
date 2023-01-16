31af648265dbe5b0fbca0dfd12bbc4677de59a4e
axel8verde <alejandro.sanchez.amor@gmail.com>
2023-01-16 20:32:05 +0100

    Nueva funcion en Utilities y cambio modelo comunidad
    Nueva funcion en Utilities y cambio modelo comunidad

--- utilities.py
+++ utilities.py
@@ -3,6 +3,8 @@ import numpy as np
 import nltk
 import re
 import string
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
 
 
 
@@ -45,3 +47,51 @@ def arrayToString(text):
 	x = ' '.join(text)
 	return x
 
+
+
+# Carga de datos
+
+def carga_datos(database):
+
+    df = database.copy()
+
+    # Limpieza de datos
+
+    # Modificar año de nacimiento a rangos de edad
+    labels=['De55a65', 'De45a55', 'De35a45', 'De25a35', 'De18a25']
+    df['year_birth'] = pd.cut(df['year_birth'], bins=[1957, 1967, 1977, 1987, 1997, 2005], labels=labels)
+    df['year_birth'] = df['year_birth'].astype(str)
+
+    # Elimino espacios en blanco de datos que me interesan para que sean una sola palabra
+    df['area'] = df['area'].apply(lambda x: re.sub(' ', '', x))
+    df['studies'] = df['studies'].apply(lambda x: re.sub(' ', '', x))
+
+    # Unir varias columnas en una sola nueva con las palabras que me interesan para el modelo
+    df['all_about_me'] = df['about_me'] + ', ' + df['mother_tongue'] + ', ' + df['area'] + ', ' + df['studies'] + ', ' + df['year_birth']
+
+    # Tokenizar las palabras de la columna all_about_me para el modelo y eliminar stopwords y signos de puntuación 
+    df['all_about_me'] = df['all_about_me'].apply(lambda x: nltk.word_tokenize(x))
+    df['all_about_me'] = df['all_about_me'].apply(lambda x: removeStopwords(x))
+    df['all_about_me'] = df['all_about_me'].apply(lambda x: removePunctuation(x))
+    df['all_about_me'] = df['all_about_me'].apply(lambda x: arrayToString(x))
+
+    # Defino el listado de opciones(usuarios) para elegir en el modelo
+    listado = df['user_id']
+
+    # Cálculo de la matriz de frecuencias de palabras
+    vectorizer = CountVectorizer()
+    MatrizFrecuencias = vectorizer.fit_transform(df['all_about_me'])
+
+    # Cálculo término frecuencia inversa de documento (TfidfTransformer())
+    transformer = TfidfTransformer()
+    tfidf = transformer.fit_transform(MatrizFrecuencias)
+
+    # Cálculo matriz similitudes
+    tdm = tfidf.transpose()
+    dtm = tfidf
+    Simil = dtm.dot(tdm)
+
+    # Visualización de la matriz de similitudes
+    SimilDF = pd.DataFrame(data = Simil.toarray(), index=listado.values,columns=listado.values)
+
+    return SimilDF
